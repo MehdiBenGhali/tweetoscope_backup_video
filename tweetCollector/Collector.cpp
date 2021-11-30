73e022cf5606a08b7b8c90f17f4098fc9cdd20bb
Ben Ghali Mehdi <mehdi.ben-ghali@student-cs.fr>
2021-11-30 15:20:19 +0000

    Update Collector.cpp

--- tweetCollector/Collector.cpp
+++ tweetCollector/Collector.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]) {
         if ( processor_map.find(tweet.source) == processor_map.end()) { //this source does not have a processor
           processor_map[tweet.source] = new tweetoscope::Processor(params, serial, sizal, tweet.source); //to each source its processor 
         }
-        processor_map[twt.source]->process(twt,cascade_id); //we run the engine corresponding to this source
+        processor_map[tweet.source]->process(tweet,cascade_id); //we run the engine corresponding to this source
     }
   }
 
